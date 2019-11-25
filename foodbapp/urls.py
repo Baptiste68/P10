354993a2863296aba01ad8f3aad93a425d9447ae
Simon Baptiste <Baptiste.Simon@swisscom.com>
2019-11-25 13:54:41 +0100

    fix urls post autocomplet

--- foodbapp/urls.py
+++ foodbapp/urls.py
@@ -19,4 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('myfoodapp/', include('myfoodapp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('myfoodapp.urls')),
+    path('/', include('myfoodapp.urls')),
 ]
